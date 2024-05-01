b9b24f51f38ae2e8e3fea3c7d6d2a32f683a5e57
hend-okasha <139397008+hend-okasha@users.noreply.github.com>
2024-05-01 22:10:19 +0200

    tasks week2

---
+++ week2/T1/src/T1.java
@@ -0,0 +1,11 @@
+public class T1 {
+    public static void main(String[] args) {
+        declare();
+        }
+    public static void declare(){
+            int ageOfPerson;
+            double heightOfBuilding;
+            boolean raining;
+            String nameOfCity;
+        }
+    }
